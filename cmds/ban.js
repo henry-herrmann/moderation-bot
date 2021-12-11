5d8664fc1e5b506d195a84de6c8366de47fa369e
Henry <henryherrmann.tv@gmail.com>
2021-12-11 17:20:25 +0100

    commit

--- cmds/ban.js
+++ cmds/ban.js
@@ -2,86 +2,85 @@ const Discord = require('discord.js');
 
 module.exports = {
     name: 'ban',
-    execute(message, client, args){
-        if(message.member.hasPermission("BAN_MEMBERS")){
-            const mention = message.mentions.users.first() || client.users.cache.get(args[0]);
-
-            if(mention){
-                var member = message.mentions.users.first(), user;
-                if(member) user = member.user;
-                if(member) user = message.guild.member(member);
-
-                if(user.bannable){
-                    if(args.length == 1){
-                        var embed = new Discord.MessageEmbed()
-                        .setTitle("You've been banned")
-                        .setColor('#000000')
-                        .setDescription("You've banned from **" + message.guild.name + "**")
-                        .setTimestamp();
-
-                        user.send(embed).then(() =>{
-                            user.ban({days: 7, reason: 'Banned from the server'});
-
-                            var ban = new Discord.MessageEmbed()
-                            .setTitle("Success!")
-                            .setColor("#0cf533")
-                            .setDescription("You successfully banned the stated user")
-                            .setTimestamp();
-
-                            message.channel.send(ban);
-                        })
-                    }else if(args.length >= 2){
-                        var reason = message.content.split(" ").splice(2).join(" ");
-
-                        var embed = new Discord.MessageEmbed()
-                        .setTitle("You've been banned")
-                        .setColor('#000000')
-                        .setDescription("You've banned from **" + message.guild.name + "** Reason: " + reason)
-                        .setTimestamp();
-
-                        user.send(embed).then(() =>{
-                            user.ban({days: 7, reason: reason});
-
-                            var ban = new Discord.MessageEmbed()
-                            .setTitle("Success!")
-                            .setColor("#0cf533")
-                            .setDescription("You successfully banned the stated user.")
-                            .setTimestamp();
-
-                            message.channel.send(ban);
-                        })
-                    }else{
-                        var embed = new Discord.MessageEmbed()
-                        .setTitle("Incorrect Usage :warning:")
-                        .setColor("#eb3434")
-                        .setDescription("Please use: **!ban @User <Reason>**")
-                        .setTimestamp();
-
-                        message.channel.send(embed);
-                    }
-                }else{
-                    var embed = new Discord.MessageEmbed()
-                    .setTitle("Unable to ban stated user :warning:")
-                    .setColor("#eb3434")
-                    .setDescription("The stated user cannot be banned by the bot. In order to ban the stated user place the bot role above the role of the stated user.")
-                    .setTimestamp();
-              
-                    message.channel.send(embed);
-                }
-            }else{
-                var embed = new Discord.MessageEmbed()
-                .setTitle("No user mentioned :warning:")
-                .setColor("#eb3434")
-                .setDescription("Please mention a user according to the syntax: **!ban @User <Reason>**")
-                .setTimestamp();
-                
-                message.channel.send(embed); 
-            }
-        }else{
+    async execute(message, args){
+        if(!message.member.hasPermission("BAN_MEMBERS")){
             var embed = new Discord.MessageEmbed()
             .setTitle("Insufficient Permissions :warning:")
             .setColor("#eb3434")
             .setDescription(`You lack the **BAN_MEMBERS** permission.`)
+            .setTimestamp();
+
+            message.channel.send(embed);
+        }
+
+        if(message.memntions.users.first() == undefined || message.mentions.users.first() == null){
+            var embed = new Discord.MessageEmbed()
+            .setTitle("No user mentioned :warning:")
+            .setColor("#eb3434")
+            .setDescription("Please mention a user according to the syntax: **!ban @User <Reason>**")
+            .setTimestamp();
+            
+            message.channel.send(embed);
+        }
+
+        var user = message.mentions.users.first(), 
+               member;
+        if(user) member = await message.guild.members.fetch(user);
+
+        if(!member.bannable){
+            var embed = new Discord.MessageEmbed()
+                .setTitle("Unable to ban stated user :warning:")
+                .setColor("#eb3434")
+                .setDescription("The stated user cannot be banned by the bot. In order to ban the stated user place the bot role above the role of the stated user.")
+                .setTimestamp();
+          
+            message.channel.send(embed);
+        }
+
+        if(args.length == 1){
+
+            var embed = new Discord.MessageEmbed()
+            .setTitle("You've been banned")
+            .setColor('#000000')
+            .setDescription("You were banned from **" + message.guild.name + "**\nReason: Banned from the server.")
+            .setTimestamp();
+
+            member.send(embed).then(() =>{
+                user.ban({days: 7, reason: "Banned."});
+
+                var ban = new Discord.MessageEmbed()
+                .setTitle("Success!")
+                .setColor("#0cf533")
+                .setDescription("You successfully banned the stated user")
+                .setTimestamp();
+
+                message.channel.send(ban);
+            })
+        }else if(args.length >= 2){
+            var reason = message.content.split(" ").splice(2).join(" ");
+
+            var embed = new Discord.MessageEmbed()
+            .setTitle("You've been banned")
+            .setColor('#000000')
+            .setDescription("You were banned from **" + message.guild.name + "**\nReason: " + reason)
+            .setTimestamp();
+
+            member.send(embed).then(() =>{
+                user.ban({days: 7, reason: reason});
+
+                var ban = new Discord.MessageEmbed()
+                .setTitle("Success!")
+                .setColor("#0cf533")
+                .setDescription("You successfully banned the stated user")
+                .setTimestamp();
+
+                message.channel.send(ban);
+            })
+        }else{
+            var embed = new Discord.MessageEmbed()
+            .setTitle("Incorrect usage :warning:")
+            .setColor("#eb3434")
+            .setDescription("Please use: **!ban @User <Reason>**")
             .setTimestamp();
 
             message.channel.send(embed);
--- cmds/kick.js
+++ cmds/kick.js
@@ -1,85 +1,88 @@
 const Discord = require('discord.js');
 
 module.exports = {
-    name: 'kick',
-    execute(message, client, args){
-        if(message.member.hasPermission('KICK_MEMBERS')){
-          const mention = message.mentions.users.first() || message.users.cache.get(args[0]);
+    name: "kick",
+    execute(message, args){
+      if(!message.member.hasPermission("KICK_MEMBERS")){
+        var embed = new Discord.MessageEmbed()
+        .setTitle("Insufficient Permissions :warning:")
+        .setColor("#eb3434")
+        .setDescription(`You lack the **KICK_MEMBERS** permission.`)
+        .setTimestamp();
 
-          var member = message.mentions.users.first(), user;
-          if(member) user = member.user;
-          if(member) user = message.guild.member(member);
+        message.channel.send(embed);
+      }
 
-          if(mention){
-
-            if(user.kickable){
-                if(args.length == 1){
-                    const embed = new Discord.MessageEmbed()
-                      .setColor("#000000")
-                      .setTitle("You have been kicked")
-                      .setDescription("You've been kicked from **"+ message.guild.name + "**")
-                      .setTimestamp();
-
-                      user.send(embed).then(() =>{
-                        user.kick("Kicked " + member.username);
-                      })
-                      
-                }else if(args.length >= 2){
-                    var reason = message.content.split(' ').splice(2).join(' ');
-
-                    var embed = new Discord.MessageEmbed()
-                      .setColor("#000000")
-                      .setTitle("You have been kicked")
-                      .setDescription("You've been kicked from **"+ message.guild.name + "** Reason: " + reason)
-                      .setTimestamp();
-
-                      user.send(embed).then(() =>{
-                        user.kick("Kicked " +member.username + " Reason: " + reason);
-
-                        var kick = new Discord.MessageEmbed()
-                        .setTitle("Success!")
-                        .setColor("#0cf533")
-                        .setDescription("Sucessfully kicked the stated user.")
-                        .setTimestamp();
-
-                        message.channel.send(kick);
-                      })
-                      
-                }else{
-                    var embed = new Discord.MessageEmbed()
-                    .setTitle("Incorrect Usage :warning:")
-                    .setColor("#eb3434")
-                    .setDescription("Please use: **!kick @User <Reason>**")
-                    .setTimestamp();
-
-                    message.channel.send(embed);
-                }
-            }else{
-              var embed = new Discord.MessageEmbed()
-              .setTitle("Unable to kick stated user :warning:")
-              .setColor("#eb3434")
-              .setDescription("The stated user cannot be kicked by the bot. In order to kick the stated user place the bot role above the role of the stated user.")
-              .setTimestamp();
-              
-              message.channel.send(embed);  
-            }
-
-          }else{
-            var embed = new Discord.MessageEmbed()
-            .setTitle("No user mentioned :warning:")
-            .setColor("#eb3434")
-            .setDescription("Please mention a user according to the syntax: **!kick @User <Reason>**")
-            .setTimestamp();
-            
-            message.channel.send(embed);
-          }
-        }else{
+      if(message.memntions.users.first() == undefined || message.mentions.users.first() == null){
           var embed = new Discord.MessageEmbed()
-          .setTitle(`Insufficient permissions :warning:`)
+          .setTitle("No user mentioned :warning:")
           .setColor("#eb3434")
-          .setDescription(`You lack the **KICK_MEMBERS** permission.`)
+          .setDescription("Please mention a user according to the syntax: **!kick @User <Reason>**")
           .setTimestamp();
+        
           message.channel.send(embed);
-        }  
+      }
+
+      var user = message.mentions.users.first(), 
+             member;
+      if(user) member = await message.guild.members.fetch(user);
+
+      if(!member.kickable){
+          var embed = new Discord.MessageEmbed()
+          .setTitle("Unable to kick the stated user :warning:")
+          .setColor("#eb3434")
+          .setDescription("The stated user cannot be kicked by the bot. In order to kick the stated user place the bots role above the role of the stated user.")
+          .setTimestamp();
+      
+          message.channel.send(embed);
+      }
+
+      if(args.length == 1){
+          var embed = new Discord.MessageEmbed()
+          .setTitle("You've been kicked!")
+          .setColor('#000000')
+          .setDescription("You were kicked from **" + message.guild.name + "**\nReason: Kicked from the server.")
+          .setTimestamp();
+
+          member.send(embed).then(() =>{
+            user.kick("Kicked.");
+
+            var kick = new Discord.MessageEmbed()
+            .setTitle("Success!")
+            .setColor("#0cf533")
+            .setDescription("You successfully kicked the stated user")
+            .setTimestamp();
+
+            message.channel.send(kick);
+        })
+      }else if(args.length >= 2){
+          var reason = message.content.split(" ").splice(2).join(" ");
+
+          var embed = new Discord.MessageEmbed()
+          .setTitle("You were kicked!")
+          .setColor('#000000')
+          .setDescription("You were kicked from **" + message.guild.name + "**\nReason: " + reason)
+          .setTimestamp();
+
+          member.send(embed).then(() =>{
+              user.kick(reason);
+
+              var kick = new Discord.MessageEmbed()
+              .setTitle("Success!")
+              .setColor("#0cf533")
+              .setDescription("You successfully kicked the stated user")
+              .setTimestamp();
+
+              message.channel.send(kick);
+          })
+      }else{
+          var embed = new Discord.MessageEmbed()
+          .setTitle("Incorrect usage :warning:")
+          .setColor("#eb3434")
+          .setDescription("Please use: **!kick @User <Reason>**")
+          .setTimestamp();
+
+          message.channel.send(embed);
+      }
     }
 }
